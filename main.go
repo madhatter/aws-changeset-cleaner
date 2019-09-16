b0b86b4f40ea6235fac06895c8003432fef7adc8
Arvid Warnecke <madhatter@nostalgix.org>
2019-09-16 17:51:12 +0200

    Working function to delete changesets but keep a given number of latest changesets.

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func (set *ChangeSet) Test() string {
 }
 
 // fetches all the stacks
-func fetchChangeSets(cfSvc *cloudformation.CloudFormation, stackName *string) ChangeSets {
+func fetchChangeSets(cfSvc *cloudformation.CloudFormation, stackName *string) (ChangeSets, error) {
 	lcsInput := cloudformation.ListChangeSetsInput{
 		StackName: stackName,
 	}
@@ -44,6 +44,7 @@ func fetchChangeSets(cfSvc *cloudformation.CloudFormation, stackName *string) Ch
 
 		if err != nil {
 			fmt.Println("Error", err)
+			return changesets, err
 		} else {
 			if output.NextToken != nil {
 				ntoken = *output.NextToken
@@ -67,15 +68,14 @@ func fetchChangeSets(cfSvc *cloudformation.CloudFormation, stackName *string) Ch
 			}
 		}
 	}
-	fmt.Println(changesets.Sets[0].ChangeSetName)
 
-	return changesets
+	return changesets, nil
 }
 
-// deletes all the changeSets for a given stackName
+// deletes all the failed changeSets for a given stackName
 func deleteChangeSets(cfSvc *cloudformation.CloudFormation, stackName string) {
 	lcsInput := cloudformation.ListChangeSetsInput{
-		StackName: aws.String("opal-inventory-ecr-live"),
+		StackName: aws.String(stackName),
 	}
 
 	ntoken := "1"
@@ -115,6 +115,7 @@ func deleteChangeSets(cfSvc *cloudformation.CloudFormation, stackName string) {
 	}
 }
 
+// deletes changeset in a given collection but keeping those newer than the given time
 func deleteChangeSetsTimeGap(cfSvc *cloudformation.CloudFormation, sets *ChangeSets, limit *time.Time) error {
 	for k, _ := range sets.Sets {
 		fmt.Println(sets.Sets[k].CreationTime)
@@ -124,14 +125,34 @@ func deleteChangeSetsTimeGap(cfSvc *cloudformation.CloudFormation, sets *ChangeS
 	return nil
 }
 
+// deletes changesets in a given collection but keeping an also given number
 func deleteChangeSetsKeep(cfSvc *cloudformation.CloudFormation, sets *ChangeSets, keep *int) error {
 	for index := 0; index < len(sets.Sets)-*keep; index++ {
-		fmt.Println(sets.Sets[index].CreationTime)
+		if sets.Sets[index].Status == "FAILED" {
+			csName := sets.Sets[index].ChangeSetName
+			stack := sets.Sets[index].StackName
+			csTime := sets.Sets[index].CreationTime
+
+			fmt.Printf("Deleting changeset %s (%s) on stack %s.\n", csName, csTime, stack)
+
+			dcsInput := cloudformation.DeleteChangeSetInput{
+				ChangeSetName: &csName,
+				StackName:     &stack,
+			}
+
+			req, err := cfSvc.DeleteChangeSet(&dcsInput)
+
+			if err != nil {
+				fmt.Println(req)
+				return err
+			}
+		}
 	}
 
 	return nil
 }
 
+// the main function
 func main() {
 	//dateForLimit := time.Now()
 	keep := 10
@@ -143,12 +164,16 @@ func main() {
 	})
 	cfSvc := cloudformation.New(sess)
 
-	sets := fetchChangeSets(cfSvc, aws.String("opal-inventory-ecr-live"))
-	//err := deleteChangeSetsTimeGap(aws.String("dv-live-developer"), &sets, &dateForLimit)
-	err := deleteChangeSetsKeep(cfSvc, &sets, &keep)
+	sets, err := fetchChangeSets(cfSvc, aws.String("opal-inventory-ecr-live"))
 
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		err := deleteChangeSetsKeep(cfSvc, &sets, &keep)
+
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	//fmt.Println(sets.Sets[1].ChangeSetId)
